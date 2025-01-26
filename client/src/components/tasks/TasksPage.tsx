8e942eef737d4b2fc59411204314b446fe6bd88d
mttetc <etchegaray.matthias@gmail.com>
2025-01-26 19:37:37 +0100

    clean task page

--- client/src/components/tasks/TasksPage.tsx
+++ client/src/components/tasks/TasksPage.tsx
@@ -11,31 +11,31 @@ import { Plus } from 'lucide-react';
 import { TaskBoard } from './TaskBoard';
 import { TaskForm } from './TaskForm';
 import { useState } from 'react';
+import { useResponsive } from '@/hooks/useResponsive';
 
 export function TasksPage() {
+  const { isDesktop } = useResponsive()
   const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="space-y-8">
-      <div className="flex justify-between items-center">
-        <h1 className="text-3xl font-bold tracking-tight">Tasks</h1>
-        <Dialog open={isOpen} onOpenChange={setIsOpen}>
-          <DialogTrigger asChild>
-            <Button>
-              <Plus className="mr-2 h-4 w-4" />
-              Add Task
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="sm:max-w-[425px]">
-            <DialogHeader>
-              <DialogTitle>Add New Task</DialogTitle>
-            </DialogHeader>
-            <DialogDescription>
-              Add a new task to your list
-            </DialogDescription>
-            <TaskForm onClose={() => setIsOpen(false)} />
-          </DialogContent>
-        </Dialog>
-      </div>
+    <div className="flex flex-col gap-4">
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogTrigger asChild>
+          <Button className={`${isDesktop ? "self-end" : ""}`}>
+            <Plus className="mr-2 h-4 w-4" />
+            Add Task
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Add New Task</DialogTitle>
+          </DialogHeader>
+          <DialogDescription>
+            Add a new task to your list
+          </DialogDescription>
+          <TaskForm onClose={() => setIsOpen(false)} />
+        </DialogContent>
+      </Dialog>
       <TaskBoard />
     </div>
   );
